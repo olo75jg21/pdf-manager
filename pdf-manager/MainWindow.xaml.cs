e1a99d292d2d8733154d020d98e54e174dc7b046
michal <m.slifierski@gmail.com>
2022-03-08 21:41:34 +0100

    'drzewko'

--- pdf-manager/MainWindow.xaml.cs
+++ pdf-manager/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,34 @@ namespace pdf_manager
       {
          InitializeComponent();
       }
-   }
+
+      private void ListDirectory(TreeView treeView, string path)
+      {
+          var rootDirectoryInfo = new DirectoryInfo(path);
+          treeView.Items.Add(CreateDirectoryNode(rootDirectoryInfo));
+      }
+
+      private static TreeViewItem CreateDirectoryNode(DirectoryInfo directoryInfo)
+      {
+          var directoryNode = new TreeViewItem { Header = directoryInfo.Name };
+          foreach (var directory in directoryInfo.GetDirectories())
+            directoryNode.Items.Add(CreateDirectoryNode(directory));
+
+          foreach (var file in directoryInfo.GetFiles())
+            if (file.Extension == ".pdf")
+                directoryNode.Items.Add(new TreeViewItem { Header = file.Name });
+
+          return directoryNode;
+      }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ListDirectory(this.Drzewko, fbd.SelectedPath);
+            }
+        }
+    }
 }
