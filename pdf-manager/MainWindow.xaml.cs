3ab26058f1c1e21da1030d048ab8aa8fb205731a
Mushroomus <pt1227@interia.pl>
2022-03-13 21:20:17 +0100

    Prototyp - Szukanie frazy - Wyswietlanie pliku PDF w przegladarce

--- pdf-manager/MainWindow.xaml.cs
+++ pdf-manager/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace pdf_manager
 {
@@ -24,5 +26,40 @@ namespace pdf_manager
       {
          InitializeComponent();
       }
-   }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "file:///C:/Users/Tomek/Desktop/test.pdf";
+            // string searchText = "Ala";
+
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder text = new StringBuilder();
+                ITextExtractionStrategy Strategy = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    string page = "";
+
+                    page = PdfTextExtractor.GetTextFromPage(reader, i, Strategy);
+                    string[] lines = page.Split('\n');
+
+                    int j = 1;
+                    foreach (string line in lines)
+                    {
+                        if (line.Contains("Ala"))
+                        {
+                            results.Items.Add("Strona: " + i + " Linia: " + j + "\n" + line + "\n");
+                        }
+                        j++;
+                    }
+                }
+            }
+        }
+
+        private void open_pdf_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\Tomek\Desktop\test.pdf");
+        }
+    }
 }
