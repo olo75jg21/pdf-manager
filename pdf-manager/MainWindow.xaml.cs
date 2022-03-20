077a8937b8c9b455ae0c414c9f9873645bd0f0bf
michal <m.slifierski@gmail.com>
2022-03-20 11:03:53 +0100

    'merged?'

--- pdf-manager/MainWindow.xaml.cs
+++ pdf-manager/MainWindow.xaml.cs
@@ -2,21 +2,10 @@
 using System.IO;
 using System.Text;
 using System.Windows;
-<<<<<<< HEAD
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
-=======
 
 /// iTextSharp
 using iTextSharp.text;
->>>>>>> Mushroomus
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 
@@ -25,7 +14,6 @@ using Xceed.Wpf.Toolkit;
 
 /// Spire 
 using Spire.Pdf;
-using System.Drawing;
 using Spire.Pdf.General.Find;
 
 
