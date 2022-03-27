dcb6222b3553f7a4321b00ee7e81b11af10aa3ee
Kuba <olo75jg12@gmail.com>
2022-03-27 18:20:57 +0200

    Add merge functionality

--- pdf-manager/MainWindow.xaml.cs
+++ pdf-manager/MainWindow.xaml.cs
@@ -15,8 +15,7 @@ using Xceed.Wpf.Toolkit;
 /// Spire 
 using Spire.Pdf;
 using Spire.Pdf.General.Find;
-
-
+using System.Windows.Forms;
 
 /// klasa wybranych pdfow do pracy
 class files_info
@@ -93,6 +92,73 @@ namespace pdf_manager
                 DirectoryTree.ListDirectory(this.Drzewko, selectedPath);
             }
         }
+
+      // Merges selected files
+      private void mergeButton_Click(object sender, RoutedEventArgs e)
+      {
+         // Need to connect list with selected items
+         SaveMergedFile(filePaths);
+      }
+
+      // Merges all files
+      private void mergeAllButton_Click(object sender, RoutedEventArgs e)
+      {
+         SaveMergedFile(filePaths);
+      }
+
+      private void SaveMergedFile(List<string> sourceList)
+      {
+         SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+         if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+         {
+            string mergePath = saveFileDialog.FileName;
+            CombineMultiplePDFs(sourceList, mergePath + ".pdf");
+         }
+      }
+
+      public static void CombineMultiplePDFs(List<string> fileNames, string outFile)
+      {
+         // step 1: creation of a document-object
+         Document document = new Document();
+         //create newFileStream object which will be disposed at the end
+         using (FileStream newFileStream = new FileStream(outFile, FileMode.Create))
+         {
+            // step 2: we create a writer that listens to the document
+            PdfCopy writer = new PdfCopy(document, newFileStream);
+
+            // step 3: we open the document
+            document.Open();
+
+            foreach (string fileName in fileNames)
+            {
+               // we create a reader for a certain document
+               using (PdfReader reader = new PdfReader(fileName))
+               {
+                  reader.ConsolidateNamedDestinations();
+
+                  // step 4: we add content
+                  for (int i = 1; i <= reader.NumberOfPages; i++)
+                  {
+                     PdfImportedPage page = writer.GetImportedPage(reader, i);
+                     writer.AddPage(page);
+                  }
+
+                  PRAcroForm form = reader.AcroForm;
+                  if (form != null)
+                  {
+                     writer.AddDocument(reader);
+                  }
+
+                  reader.Close();
+               }
+            }
+
+            // step 5: we close the document and writer
+            writer.Close();
+            document.Close();
+         }//disposes the newFileStream object
+      }
 
       // ELDEN RING ENJOYER FUNCTION CONENCTED WITH ELDEN RING ENJOYER COMMENTED WRAP PANEL
       /*
