064596956242c7720a3ad5e47e58fc723d2137e2
michal <m.slifierski@gmail.com>
2022-03-28 15:59:05 +0200

    added essential files

---
+++ pdf-manager/SelectedFilesList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf_manager
+{
+    internal class FilePath
+    {
+        private string _Path;
+        public string Path { get { return _Path; } set { _Path = value; } }
+        public string Name { get { return System.IO.Path.GetFileName(_Path); } }
+    }
+}
